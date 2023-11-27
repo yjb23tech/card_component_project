22c6368170ce38aa8b570d81b2f1e5db8fab5aad
yjb23tech <yjb23tech@gmail.com>
2023-11-27 16:18:12 +0000

    Banking on me

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,10 +1,22 @@
 import card_photo from "../images/kanye-west-graduation.jpg"
+import phone_icon from "../images/phone-icon.png"
+import mail_icon from "../images/mail-icon.png"
 
 const Card = () => {
     return (
-        <div>
+        <div className="contact-card">
             <img src={card_photo} />
+            <h3>The Graduation</h3>
+            <div className="info-group">
+                <img src={phone_icon} />
+                <p>(212) 555-1234</p>
+            </div>
+            <div className="info-group">
+                <img src={mail_icon} />
+                <p>kanye_west@goodmusicrecords.com</p>
+            </div>
         </div>
     );
 };
 
+export default Card;
